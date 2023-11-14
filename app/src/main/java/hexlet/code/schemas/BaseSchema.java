8241b98f9b287e38aa77b160a367b988c725ac03
Aleksandr Muzalev <muzalev.as@gmil.com>
2023-11-14 05:01:50 +0300

    Step 5.2

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -2,6 +2,7 @@ package hexlet.code.schemas;
 
 public abstract class BaseSchema {
 
+    /**Method can be safely overridden*/
     public boolean isValid(Object input) {
         return true;
     }
