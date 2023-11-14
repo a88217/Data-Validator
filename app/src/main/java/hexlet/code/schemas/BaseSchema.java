c08afc7f317dbf27c92b367903d7a98ef8010eb6
Aleksandr Muzalev <muzalev.as@gmil.com>
2023-11-14 06:01:52 +0300

    Step 5.3 Checkstyle

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -4,6 +4,8 @@ public abstract class BaseSchema {
 
     /**
      * Method can be safely overridden.
+     * @param input Validated object
+     @return some boolean value.
      * */
     public boolean isValid(Object input) {
         return true;
